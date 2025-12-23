ce9dd264582c132c64eb70b1e24356b429e9336f
daehyeonkim0326 <daehyeonkim0326@gmail.com>
2025-12-23 17:50:08 +0900

    cartpage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,10 +1,29 @@
 import CartContents from "../section/cart/CartContents";
+import { IoIosArrowDropdown } from "react-icons/io";
+import { RiSofaLine } from "react-icons/ri";
+import { IoMdSearch } from "react-icons/io";
+import { CgShoppingCart } from "react-icons/cg";
+
+
+import "./cart.scss";
+
 
 const CartPage = () => {
   return (
     <div id="cart-page">
-      <p>MY CART</p>
-      <CartContents />
+      <div className="top">
+        <h1>MY CART</h1>
+      </div>
+      <div className="middle">
+        <CartContents />
+      </div>
+      <div className="buy">
+        <div>
+          <h3>￦</h3> 
+          <p>Total</p>
+        </div>
+        <button>결제하기</button>
+      </div>
     </div>
   )
 }
