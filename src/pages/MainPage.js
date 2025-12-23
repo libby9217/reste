bed89aba74a35cebd06daf3b376daecdf92a39ff
BroHsol <brohsol93@gmail.com>
2025-12-23 18:26:55 +0900

    header

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,12 +2,10 @@ import MainVideo from "../section/main/MainVideo";
 import ProductMD from "../section/main/ProductMD";
 import BestItem from "../section/main/BestItem";
 import BrandPR from "../section/main/BrandPR";
-import { IoIosArrowDropdown } from "react-icons/io";
 
 const MainPage = () => {
   return (
     <div id="main-page">
-      <IoIosArrowDropdown />
       <MainVideo />
       <ProductMD />
       <BestItem />
--- src/section/common/Header.js
+++ src/section/common/Header.js
@@ -1,23 +1,28 @@
 import { RiSofaLine } from "react-icons/ri";
 import { IoSearch } from "react-icons/io5";
 import { FiShoppingCart } from "react-icons/fi";
+import { IoIosArrowDropdown } from "react-icons/io";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <div id="header">
+      <div className="icon-drop">
+        <IoIosArrowDropdown className="icon" />
+      </div>
       <nav>
         <div className="icon-sofa">
           <RiSofaLine className="icon" />
         </div>
         <div className="search">
           <input type="text" placeholder="검색"/>
-          <IoSearch className="icon-search"/>
+          <IoSearch className="icon"/>
         </div>
         <div className="icon-cart">
           <FiShoppingCart className="icon" />
         </div>
       </nav>
+
     </div>
   )
 }
