2f5bdafc6f67f2b6673b8fd93cc99aa63242a995
BroHsol <brohsol93@gmail.com>
2025-12-24 19:37:53 +0900

    Best Item update

--- src/section/main/BestItem.js
+++ src/section/main/BestItem.js
@@ -1,4 +1,23 @@
-import './BestItem.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Mousewheel } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import './BestItem.scss';
+import { Link } from "react-router-dom";
+import img1 from "../../assets/img/Veil_01.png";
+import img2 from "../../assets/img/Still_01.png";
+import img3 from "../../assets/img/CHROME_HAVEN_01.png";
+import img4 from "../../assets/img/Cloud_01.png";
+import img5 from "../../assets/img/Velora_01.png";
+import img6 from "../../assets/img/Midnight_05.png";
+const slideData = [
+  { id: 1, img: img1, title: "Veil"},
+  { id: 2, img: img2, title: "Still"},
+  { id: 3, img: img3, title: "CHROME_HAVEN"},
+  { id: 4, img: img4, title: "Cloud"},
+  { id: 5, img: img5, title: "Velora"},
+  { id: 6, img: img6, title: "Midnight"},
+];
 
 const BestItem = () => {
 
